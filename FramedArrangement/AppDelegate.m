5775f094614691e5e210caba2270179da2e703e9
Joshua Howland <development@jkhowland.com>
2014-12-23 15:02:33 -0700

    Initial Commit

---
+++ FramedArrangement/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  FramedArrangement
+//
+//  Created by Joshua Howland on 12/23/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
